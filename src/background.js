e1368b8278f57da21144d86eef5543441ee843a0
Igor <icastano012@ikasle.ehu.eus>
2026-02-06 13:35:45 +0100

    v1.7

--- src/background.js
+++ src/background.js
@@ -1,6 +1,14 @@
-// Background service worker
-// Currently minimal, but enables proper extension lifecycle handling.
-
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Map Collaborator installed.');
+});
+
+// Fix for "The service worker navigation preload request was cancelled" warning
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      if (self.registration.navigationPreload) {
+        await self.registration.navigationPreload.disable();
+      }
+    })()
+  );
 });
